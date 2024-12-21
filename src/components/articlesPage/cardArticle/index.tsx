e53c92b2a642293dfaa3800f248fb6cc19fd4420
joabesnts09 <joabesilva1233@gmail.com>
2024-12-20 23:57:03 -0300

    fix: position of buttons on card

--- src/components/articlesPage/cardArticle/index.tsx
+++ src/components/articlesPage/cardArticle/index.tsx
@@ -25,38 +25,40 @@ export const CardArticle = ({
   return (
     <>
       <div className='max-w-md mx-auto bg-background border border-borderColor rounded-lg shadow-lg overflow-hidden flex flex-col'>
-  <div className='relative h-48 bg-secondary'>
-    <Image
-      src={image ? image : 'https://via.placeholder.com/400x200'}
-      className='rounded-t-md object-cover'
-      alt={name}
-      fill
-      priority
-      unoptimized
-    />
-  </div>
+        <div className='relative h-48 bg-secondary'>
+          <Image
+            src={image ? image : 'https://via.placeholder.com/400x200'}
+            className='rounded-t-md object-cover'
+            alt={name}
+            fill
+            priority
+            unoptimized
+          />
+        </div>
 
-  <div className='flex flex-col flex-1 p-5 text-foreground'>
-    <h2 className='text-textSecondary text-2xl font-bold mb-2'>{name}</h2>
-    <p className='text-textPrimary text-sm font-bold italic mb-2'>{date}</p>
-    <p className='mb-4 font-semibold'>{description}</p>
-  </div>
+        <div className='flex flex-col flex-1 p-5 text-foreground'>
+          <h2 className='text-textSecondary text-2xl font-bold mb-2'>{name}</h2>
+          <p className='text-textPrimary text-sm font-bold italic mb-2'>
+            {date}
+          </p>
+          <p className='mb-4 font-semibold'>{description}</p>
+        </div>
 
-  <div className='flex items-center justify-start gap-4 p-5 bg-background '>
-    <SocialButton
-      href='https://www.kwai.com'
-      text='Assista'
-      className='bg-orange-500 hover:bg-orange-600'
-      icon={<KwaiIcon className='w-6 h-6' />}
-    />
-    <SocialButton
-      href='https://www.tiktok.com'
-      text='Assista'
-      className='bg-black hover:bg-gray-800'
-      icon={<TiktokIcon className='w-6 h-6' />}
-    />
-  </div>
-</div>
+        <div className='flex items-center justify-start gap-4 p-5 bg-background '>
+          <SocialButton
+            href={kwaiLink}
+            text='Assista'
+            className='bg-orange-500 hover:bg-orange-600'
+            icon={<KwaiIcon className='w-6 h-6' />}
+          />
+          <SocialButton
+            href={tiktokLink}
+            text='Assista'
+            className='bg-black hover:bg-gray-800'
+            icon={<TiktokIcon className='w-6 h-6' />}
+          />
+        </div>
+      </div>
     </>
   )
 }
